1767c0ae6678782b2200d962e10a60398ecb109e
Vincent Breitmoser <look@my.amazin.horse>
2017-12-15 17:55:46 +0200

    use pointers for referencing go classes

--- key.go
+++ key.go
@@ -32,33 +32,33 @@ type Key struct {
 }
 
 // GenKey creates a new ecdh key
-func GenKey() (Key, error) {
+func GenKey() (*Key, error) {
 	key, err := ecdh.NewKeypair(rand.Reader)
 	if err != nil {
-		return Key{}, err
+		return &Key{}, err
 	}
 	return buildKey(key), nil
 }
 
 // StringToKey builds a Key from a string
-func StringToKey(keyStr string) (Key, error) {
+func StringToKey(keyStr string) (*Key, error) {
 	var key ecdh.PrivateKey
 
 	keyBytes, err := hex.DecodeString(keyStr)
 	if err != nil {
-		return Key{}, err
+		return &Key{}, err
 	}
 
 	err = key.FromBytes(keyBytes)
 	if err != nil {
-		return Key{}, err
+		return &Key{}, err
 	}
 
 	return buildKey(&key), nil
 }
 
-func buildKey(key *ecdh.PrivateKey) Key {
-	return Key{
+func buildKey(key *ecdh.PrivateKey) *Key {
+	return &Key{
 		Private: hex.EncodeToString(key.Bytes()),
 		Public:  key.PublicKey().String(),
 		priv:    key,
